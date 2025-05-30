f7b886cd66c5038a320cff07a8277fc7478a6653
Krishna Sathvik <krishnasathvikmantripragada@gmail.com>
2025-05-30 11:05:05 -0500

    fix code for multiple files v1

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import ScrollToTopButton from "./ScrollToTopButton";
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col font-sans text-gray-900 bg-gradient-to-br from-white via-pink-50 to-pink-100">
+    <div className="min-h-screen w-full overflow-hidden bg-gradient-to-br from-white via-pink-50 to-white">
       <main className="flex-grow main-scroll max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         {children}
       </main>
